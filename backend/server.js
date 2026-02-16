f7ceaaee50748e8b49df2739f39b7ac2f69c600b
Shehan  Lokuwella <155091477+Shehan013@users.noreply.github.com>
2026-02-16 23:23:54 +0530

    feat: setup basic Express server with tesitng endpoint

---
+++ backend/server.js
@@ -0,0 +1,30 @@
+require('dotenv').config();
+const express = require('express');
+const cors = require('cors');
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(cors({
+    origin: process.env.FRONTEND_URL || 'http://localhost:5173',
+    credentials: true
+}));
+app.use(express.json());
+
+
+app.get('/api/weather', (req, res) => {
+    res.json({ status : 'ok', message: 'Weather data endpoint is working!' });
+});
+
+app.use((err, req, res, next) => {
+    console.error(err.stack);
+    res.status(500).json({ error: 'Internal Server Error' });
+});
+
+app.listen(PORT, () => {
+    console.log(`Backend server is running on port ${PORT}`);
+});
+
+
+
+
