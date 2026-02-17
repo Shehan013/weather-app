9c16ac1f6e3741fe43be4eb2ebcba88b83f421e8
Shehan  Lokuwella <155091477+Shehan013@users.noreply.github.com>
2026-02-17 22:01:39 +0530

    fix: make weather endpoints public

--- backend/routes/weather.routes.js
+++ backend/routes/weather.routes.js
@@ -1,9 +1,8 @@
 const express = require('express');
 const router = express.Router();
 const weatherController = require('../controllers/weather.controller');
-const { checkJwt, attachUser } = require('../middleware/auth.middleware');
 
-router.get('/cities', checkJwt, attachUser, weatherController.getCities);
+router.get('/cities', weatherController.getCities);
 router.get('/debug/cache', weatherController.getCacheStatus);
 
 module.exports = router;
